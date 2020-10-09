b8e5adb9cdd3029399ed43c5b4877e3af39156d0
Maxiboii <67202298+Maxiboii@users.noreply.github.com>
2020-10-09 09:03:46 +0300

    Navbar

--- map/views.py
+++ map/views.py
@@ -1,24 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
+from ads.owner import OwnerDetailView, OwnerDeleteView
 from django.conf import settings
-from map.models import Population, CasesToday, Utility, Map
+from map.models import Population, CasesToday, Utility, Map, MapComment
+from map.forms import CommentForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
-# Create your views here.
-class MapView(View):
-    def get(self, request):
-        print(request.get_host())
-        host = request.get_host()
-        islocal = host.find('localhost') >= 0 or host.find('127.0.0.1') >= 0
+
+class MapView(OwnerDetailView):
+    model = Map
+    template_name = "map/index.html"
+    def get(self, request) :
+        comments = MapComment.objects.all().order_by('-updated_at')
+        comment_form = CommentForm()
         population = Population.objects.all()
         casestoday = CasesToday.objects.all()
         util = Utility.objects.all()
         map = Map.objects.all()
         context = {
-            'installed': settings.INSTALLED_APPS,
-            'islocal': islocal,
-            'population': population,
-            'cases': casestoday,
-            'util': util,
-            'map': map
+        'comments': comments,
+        'n': len(comments),
+        'comment_form': comment_form,
+        'population': population,
+        'cases': casestoday,
+        'util': util,
+        'map': map
         }
-        return render(request, 'map/index.html', context)
+        return render(request, self.template_name, context)
+
+
+class CommentCreateView(LoginRequiredMixin, View):
+    def post(self, request) :
+        comment = MapComment(text=request.POST['comment'], owner=request.user)
+        comment.save()
+        return redirect(reverse('map:map'))
+
+
+class CommentDeleteView(OwnerDeleteView):
+    model = MapComment
+    template_name = "map/comment_delete.html"
+
+    def get_success_url(self):
+        return reverse('map:map')
--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Used for a default title
-APP_NAME = 'ChucksList'
+APP_NAME = 'Maxiboi'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
